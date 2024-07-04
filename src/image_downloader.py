6495b9cca9c0c438cc48efb36967289d9fe80da4
Vinitziin <vinicius.tiago.65154@a.fecaf.com.br>
2024-07-04 08:58:11 -0300

    Ajuste de variaveis

--- src/image_downloader.py
+++ src/image_downloader.py
@@ -1,10 +1,10 @@
 import requests
 
-def check_image_available(url):
+def verificar_imagem_disponivel(url):
     response = requests.head(url, verify=False)
     return response.status_code == 200
 
-def download_and_save_map(url, file_path):
+def baixar_e_salvar_imagem(url, file_path):
     response = requests.get(url, verify=False)
     if response.status_code == 200:
         with open(file_path, 'wb') as f:
@@ -12,7 +12,7 @@ def download_and_save_map(url, file_path):
         return True
     return False
 
-def build_url(date, hour):
+def construir_url(date, hour):
     base_url = "https://www.tropicaltidbits.com/analysis/models/gfs/"
     formatted_date = date.strftime('%Y%m%d')
     return f"{base_url}{formatted_date}{hour}/gfs_apcpn_samer_64.png"
